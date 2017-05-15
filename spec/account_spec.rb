f7c1d439b544a2dd86f4d8f25d22e0a5b94070d0
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-05-15 11:33:34 +0100

    Third test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -13,4 +13,10 @@ describe Account do
     expect(account.balance).to eq 1000
   end
 
+  it "Can withdrawal money and it deduct it from the account" do
+    account.deposit(1000)
+    account.withdrawal(500)
+    expect(account.balance).to eq 500
+  end
+  
 end
