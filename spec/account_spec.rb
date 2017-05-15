3650e95c3fc89c45a104582739e2cc34d0921d8c
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-05-15 11:41:14 +0100

    Add edge case test, negative adeposit

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -13,10 +13,15 @@ describe Account do
     expect(account.balance).to eq 1000
   end
 
+  it "Cant' add a negative deposit amount to the account" do
+    account.deposit(-500)
+    expect(account.balance).to raise_error "You can't add a negative amount love"
+  end
+
   it "Can withdrawal money and it deduct it from the account" do
     account.deposit(1000)
     account.withdrawal(500)
     expect(account.balance).to eq 500
   end
-  
+
 end
